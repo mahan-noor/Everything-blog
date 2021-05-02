51d4e388f8304a8adc672ab920088359ec2a1bfc
mahan-noor <ismahanabey@gmail.com>
2021-05-02 16:06:59 +0300

    create a comment class and initialize migration

--- app/models.py
+++ app/models.py
@@ -6,6 +6,11 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    
+    comment = db.relationship('Comment',backref = 'role',lazy="dynamic")
+
 
 
     def __repr__(self):
@@ -23,3 +28,12 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+
+
+
