0993a888723f798b4da7c34c7604d9f418840f1f
mahan-noor <ismahanabey@gmail.com>
2021-05-02 11:54:29 +0300

    create a login and register html files

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ class Config:
 
  QUOTES_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
  SECRET_KEY=os.environ.get('SECRET_KEY')
- SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://lenovo:1234@localhost/e-blog'
+ SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://lenovo:1234@localhost/blog'
 
 
 class ProdConfig(Config):
-    
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     pass
