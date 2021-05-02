d19f3754a700e0e0bcfd9ccd5b1fc638d3e34ddc
mahan-noor <ismahanabey@gmail.com>
2021-05-02 09:13:07 +0300

    add a create app function and create a blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
 # Initializing application
 app = Flask(__name__)
 
-from app import views
+def create_app(config_name):
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
--- config.py
+++ config.py
@@ -2,5 +2,19 @@ import os
 
 
 class Config:
-    
- SECRET_KEY = os.environ.get('SECRET_KEY')
+
+ QUOTES_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
+ SECRET_KEY=os.environ.get('SECRET_KEY')
+
+
+ class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
