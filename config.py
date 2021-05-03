da7af8c817bfc54c8be4f5ce361049029c82b286
mahan-noor <ismahanabey@gmail.com>
2021-05-03 15:16:59 +0300

    add mail configuration and export in start

--- config.py
+++ config.py
@@ -2,10 +2,18 @@ import os
 
 
 class Config:
+    
+    QUOTES_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://lenovo:1234@localhost/blog'
+    
 
- QUOTES_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
- SECRET_KEY=os.environ.get('SECRET_KEY')
- SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://lenovo:1234@localhost/blog'
+ #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export SECRET_KEY='f283a81077b38ac239b6d1be1a511c261f9ea46c4aeb47ac'
-
+export MAIL_USERNAME='mahannoor850@gmail.com'
+export MAIL_PASSWORD='12345'
 
 python3.8 manage.py server
