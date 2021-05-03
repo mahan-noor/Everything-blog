0eec5d02483205855fb9d00919ae9b79d417bc35
mahan-noor <ismahanabey@gmail.com>
2021-05-03 07:54:48 +0300

    add  class  quote

--- app/models.py
+++ app/models.py
@@ -120,6 +120,15 @@ class Comment(db.Model):
     
 
     
+class Quote:
+    """
+    Quote class to define quote objects
+    """
 
+    def __init__(self,author,id,quote,permalink):
+        self.author = author
+        self.id = id
+        self.quote = quote
+        self.permalink = permalink
 
 
